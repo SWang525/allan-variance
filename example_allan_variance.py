77129e55039743410e93defa39fdf164c38b1cee
ShWang <shaocheng.wang@nauto.com>
2018-05-25 17:54:49 -0700

    snapshot

--- example_allan_variance.py
+++ example_allan_variance.py
@@ -17,16 +17,22 @@ def main():
     q_walk = 0.05
     q_ramp = 0.001
     signal = generate_signal(num_samples, dt, q_white=q_white, q_walk=q_walk, q_ramp=q_ramp)
-    plt.plot(time, signal)
-    plt.show()
-
     tau, av = allan_variance(x=signal, dt=dt, min_cluster_size=1, min_cluster_count='auto',
                              n_clusters=100, input_type="increment", verbose=True)
-    plt.loglog(tau, av, '.')
-    plt.grid(True)
+
+    f, axarr = plt.subplots(2)
+    axarr[0].plot(time, signal)
+    axarr[0].grid(True)
+    axarr[0].set_xlabel("Time (s)")
+    axarr[0].set_ylabel("X(t)")
+
+    axarr[1].loglog(tau, av, '.')
+    axarr[1].grid(True)
+    axarr[1].set_xlabel("tau")
+    axarr[1].set_ylabel("AVAR(tau)")
+
     params, av_pred = params_from_avar(tau, av, output_type='dict')
     print params
-
 
     plt.loglog(tau, av_pred)
     plt.show()
